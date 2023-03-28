ddfe4eba5f3b47432bbdf45cb7591f697807c70a
suijiyuan <suijiyuan@outlook.com>
2023-03-28 16:23:09 +0800

    feat: more logs in red-bull.log rather than nohup.log

--- bin/start.sh
+++ bin/start.sh
@@ -30,9 +30,9 @@ if
     -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:10002 \
     -Djasypt.encryptor.password="$1" \
     -jar "$ROOT_PATH/red-bull-0.0.1-SNAPSHOT.jar" \
-    --spring.profiles.active=prod >>"$HOME/logs/$APP/nohup.out" 2>&1 &
+    --spring.profiles.active=prod >>"$HOME/logs/$APP/$APP.log" 2>&1 &
 then
-  echo "$APP is running, more logs: $HOME/logs/$APP/nohup.out"
+  echo "$APP is running, more logs: $HOME/logs/$APP/$APP.log"
 else
-  echo "$APP start failed, more logs: $HOME/logs/$APP/nohup.out"
+  echo "$APP start failed, more logs: $HOME/logs/$APP/$APP.log"
 fi
