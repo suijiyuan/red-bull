572d7403098304cd775dcd9f8ed4ad45fa3193a9
suijiyuan <suijiyuan@outlook.com>
2023-12-04 11:16:07 +0800

    fix: fix warning of unknown field authorities

--- src/main/java/xyz/outerringroad/redbull/convert/UserConverter.java
+++ src/main/java/xyz/outerringroad/redbull/convert/UserConverter.java
@@ -1,6 +1,7 @@
 package xyz.outerringroad.redbull.convert;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 import xyz.outerringroad.redbull.bean.bo.UserBO;
 import xyz.outerringroad.redbull.bean.po.UserPO;
@@ -10,6 +11,7 @@ public interface UserConverter {
 
     UserConverter INSTANCE = Mappers.getMapper(UserConverter.class);
 
+    @Mapping(target = "authorities", ignore = true)
     UserBO convertUserPOToUserBO(UserPO userPO);
 
 }
